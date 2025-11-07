1d75e8faa735065b06a4b6e11597e6f264d16ba7
dprabakar1506 <dprabakar@binghamton.edu>
2025-11-07 18:31:53 +0530

    Create taumBday.java

---
+++ Fall/Week5/taumBday.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'taumBday' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER b
+     *  2. INTEGER w
+     *  3. INTEGER bc
+     *  4. INTEGER wc
+     *  5. INTEGER z
+     */
+
+    public static long taumBday(int b, int w, int bc, int wc, int z) {
+    // Write your code here
+    long costBlackDirect = bc;
+        long costWhiteDirect = wc;
+
+        // Calculate the cost of buying black gifts by converting white gifts
+        if (wc + z < bc) {
+            costBlackDirect = wc + z;
+        }
+
+        // Calculate the cost of buying white gifts by converting black gifts
+        if (bc + z < wc) {
+            costWhiteDirect = bc + z;
+        }
+
+        return (b * costBlackDirect) + (w * costWhiteDirect);
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int b = Integer.parseInt(firstMultipleInput[0]);
+
+                int w = Integer.parseInt(firstMultipleInput[1]);
+
+                String[] secondMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int bc = Integer.parseInt(secondMultipleInput[0]);
+
+                int wc = Integer.parseInt(secondMultipleInput[1]);
+
+                int z = Integer.parseInt(secondMultipleInput[2]);
+
+                long result = Result.taumBday(b, w, bc, wc, z);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
