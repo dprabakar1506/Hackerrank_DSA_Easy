774d189bcf1abe2a002a6549c4d7008f48f9b0a9
dprabakar1506 <dprabakar@binghamton.edu>
2025-11-07 18:15:46 +0530

    Create Lisa'sWorkbook.java

---
+++ Week6/Lisa'sWorkbook.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'workbook' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER k
+     *  3. INTEGER_ARRAY arr
+     */
+
+    public static int workbook(int n, int k, List<Integer> arr) {
+    // Write your code here
+    int page = 1;
+        int specialProblems = 0;
+
+        for (int problemsPerChapter : arr) { // Iterating through problems in each chapter
+            for (int p = 1; p <= problemsPerChapter; p++) {
+                if (p == page) {
+                    specialProblems++;
+                }
+                if (p % k == 0 && p != problemsPerChapter) { // Page is full and not end of chapter
+                    page++;
+                }
+            }
+            page++; // New chapter starts on a new page
+        }
+        return specialProblems;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.workbook(n, k, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
